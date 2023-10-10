9e76f8d25ecd64c40824d770ff4158f68583cebe
root <besckinder@gmail.com>
2023-10-09 20:25:51 -0700

    Base Model done

---
+++ models/base_model.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+''' This module contains the base model that all other classes inherit from '''
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    ''' Base class '''
+
+    def __init__(self):
+        ''' instantization '''
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        ''' returns a printable string of the object '''
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        ''' Updates the public instance attribute `updated_at with the cuurent
+            datetime. '''
+
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        ''' returns a dictionary containing all keys, values of __dict__ of the
+            instance. '''
+        
+        Dict = {}
+        for key, value in self.__dict__.items():
+            if value is not None:
+                if key == "created_at" or key == "updated_at":
+                    Dict[key] = datetime.isoformat(value)
+                else:
+                    Dict[key] = value
+        return Dict
