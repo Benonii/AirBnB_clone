6284bfe47c67ef2f41596a125ef2c64aebd5252f
Eman288 <emantamam107@gmail.com>
2023-10-13 18:43:27 -0700

    to_dict tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -99,9 +99,69 @@ class TestBase_save(unittest.TestCase):
         self.assertNotEqual(time, time2)
 
 class TestBase_dict(unittest.TestCase):
-    '''a class to test the dict function'''
+    '''a class to test the to_dict function'''
 
-    pass
+    def test_isAttributeThere(self):
+        '''checks if the directory have all of the attributes of the class'''
+
+        m = BaseModel()
+        self.assertIn("id", m.to_dict())
+        self.assertIn("created_at", m.to_dict())
+        self.assertIn("updated_at", m.to_dict())
+        self.assertIn("__class__", m.to_dict())
+
+    def test_canAddAttributes(self):
+        '''checks if the directory have the new created attributes'''
+
+        m = BaseModel()
+        m.first_name = "Jack"
+        m.last_name = "Will"
+        m.number = 28
+        self.assertIn("first_name", m.to_dict())
+        self.assertIn("last_name", m.to_dict())
+        self.assertIn("number", m.to_dict())
+
+    def test_isRightValues(self):
+        '''checks if the directory have the right values'''
+
+        m = BaseModel()
+        m.first_name = "Jack"
+        m.last_name = "Will"
+        m.number = 28
+        self.assertIsEqual("Jack", m.to_dict()["first_name"])
+        self.assertIsEqual("Will", m.to_dict()["last_name"])
+        self.assertIsEqual(28, m.to_dict()["number"])
+    
+    def test_dictIsDict(self):
+        '''checks if the dict's return value's type is dict'''
+
+        m = BaseModel()
+        self.assertEqual(dict, type(m.to_dict()))
+
+    def test_classIsTheNameOfTheClass(self):
+        '''checks if the __class__ has the name of the object'''
+
+        m = BaseModel()
+        self.assertIsEqual("BaseModel", m.to_dict()["__class__"])
+
+    def test_rightReturn(self):
+        '''checks if the to_dict function returns the correct answer'''
+
+        m = BaseModel()
+        time = m.created_at
+        m.id = "28"
+        sleep(1)
+        m.save()
+        m.created_at = time
+        m.updated_at = time
+        time = time.isoformat()
+        dictionary = {
+                'id': '28',
+            '__class__': 'BaseModel',
+            'created_at': time,
+            'updated_at': time,
+            }
+        self.asserIsEqual(dictionary, m.to_dict())
 
 if __name__ == "__main__":
     unittest.main()
