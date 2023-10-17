d28f2a2ead6edf44b7d8b2bc7a39d264e53fb223
root <besckinder@gmail.com>
2023-10-16 19:06:11 -0700

    Changed user a little bit

--- models/user.py
+++ models/user.py
@@ -10,9 +10,9 @@ class User(BaseModel):
 
     def __init__(self, *args, **kwargs):
         ''' Instantization '''
+        super().__init__(*args, **kwargs)
+
         self.email = ""
         self.password = ""
         self.first_name = ""
         self.last_name = ""
-
-        super().__init__(*args, **kwargs)
