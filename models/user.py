67290be9de4e69e05a9a0adba8de80630209bc53
root <besckinder@gmail.com>
2023-10-16 19:12:29 -0700

    removing self. from class attr

--- models/user.py
+++ models/user.py
@@ -8,10 +8,10 @@ from models.base_model import BaseModel
 class User(BaseModel):
     ''' A class that inherits from BaseModel '''
 
-    self.email = ""
-    self.password = ""
-    self.first_name = ""
-    self.last_name = ""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
 
     def __init__(self, *args, **kwargs):
         ''' Instantization '''
