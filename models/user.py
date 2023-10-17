ab0f3605877af8a2189bac59759af9761498fcab
root <besckinder@gmail.com>
2023-10-16 19:09:51 -0700

    Changed user a little bit

--- models/user.py
+++ models/user.py
@@ -8,11 +8,11 @@ from models.base_model import BaseModel
 class User(BaseModel):
     ''' A class that inherits from BaseModel '''
 
+    self.email = ""
+    self.password = ""
+    self.first_name = ""
+    self.last_name = ""
+
     def __init__(self, *args, **kwargs):
         ''' Instantization '''
         super().__init__(*args, **kwargs)
-
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
